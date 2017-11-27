8c4e1f2ba9b402ad58678675e038888edfe7b6c3
Ivan Pronin <ivanp@playtika.com>
2017-11-27 16:44:19 +0300

    Add decorator pattern

---
+++ java/design-patterns/src/main/groovy/DesingPatternsDescription.groovy
@@ -0,0 +1,18 @@
+/**
+ *
+ * Created by ivanp on 27.11.2017
+ */
+class DesingPatternsDescription {
+
+    /**
+     * Defines a family of algorithms, encapsulates each one, and makes them interchangable.
+     * Strategy lets the algorithm vary independently from  clients that use it
+     */
+    void strategy() {}
+
+    /**
+     * Defines a one-to-many dependency between objects so that when one object changes state,
+     * all of its dependants are notified and updated automatically
+     */
+    void observer() {}
+}
---
+++ java/design-patterns/src/main/groovy/OODesignPrinciples.groovy
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by ivanp on 27.11.2017
+ */
+class OODesignPrinciples {
+
+    // Encapsulate what varies
+    // Favor composition over inheritance
+    // Program to interfaces, not implementations
+    // Strive for loosely coupled designs between objects that interact
+    // Classes should be open for extension, but closed for modification
+}
